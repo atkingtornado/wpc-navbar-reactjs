97fa1336431397d643de008826fc56b337f42c40
Austin King <atkingtornado@gmail.com>
2022-12-29 12:47:08 -0700

    changed up npm package/component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface HeaderProps {
+
+}
+
+const Header = ({ }: HeaderProps) => {
+    
+    return (
+        <div>
+            <p>test</p>
+        </div>
+    )
+}
+
+export default Header;
