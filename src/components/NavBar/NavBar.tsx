6a08e0fa9cd6f5fa6c4ee4312ae414c83fe27ef3
Austin King <atkingtornado@gmail.com>
2023-01-10 11:57:53 -0700

    update styling

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -37,109 +37,148 @@ const centerLinks = [
   ['WPC', 'https://www.wpc.ncep.noaa.gov/'],
 ]
 
-interface NavBarProps {
 
+const useViewport = () => {
+  const [width, setWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
+  // Return the width so we can use it in our components
+  return { width };
 }
 
-export const NavBar = ({ }: NavBarProps) => (
-  <AppBar 
-    position="static"
-    sx={{
-      background: 'white',
-      color: 'black'
-    }}
-  >
+interface NavBarProps {}
+export const NavBar = (NavBarProps) => {
 
-    <Box 
-      display="flex" 
-      flexDirection="row" 
-      justifyContent="center"
-      alignContent="start"
+  const { width } = useViewport();
+  const breakpoint = 750;
+
+  return (
+    <AppBar 
+      position="static"
+      sx={{
+        background: 'white',
+        color: 'black',
+        'text-align': 'center',
+        'box-shadow': 'none'
+      }}
     >
-        <Box
-          display="flex" 
-          alignContent="start"
-          // position="absolute"
-          // left="5px"
-        >
-         <img src={headerNWS} />
-         <img src={headerNOAA} />
-        </Box>
-       <Box display="flex" flexDirection="column" padding={0} marginLeft={1}>
-         <Typography
-          noWrap
-          component="a"
-          href="/"
-          sx={{
-            fontFamily: 'Arial,sans-serif',
-            fontSize: '2.0em',
-            letterSpacing: '0px',
-            wordSpacing: '-0.5px',
-            textDecoration: 'none',
-            fontWeight: '550',
-            color: 'black',
-            textTransform: 'uppercase',
-            fontVariant: 'small-caps',
-          }}
-        >
-          Weather Prediction Center
-        </Typography>
-        <Typography
-          noWrap
-          component="a"
-          href="/"
-          sx={{
-            fontVariant: 'small-caps',
-            textTransform: 'uppercase',
-            fontFamily: 'Arial,sans-serif',
-            fontSize: '0.7em',
-            textDecoration: 'none',
-            color: 'gray',
-            letterSpacing: '3px'
-          }}
-        >
-          National Oceanic and Atmospheric Administration
-        </Typography>
-      </Box>
-      <Box
-        // position="absolute"
-        // right="5px"
+
+      <Box 
+        display="flex" 
+        flexDirection="row" 
+        justifyContent="center"
+        alignContent="start"
       >
-        <img src={headerDOC} />
-      </Box>
-    </Box>
-    <Divider />
-    <Box display="flex" flexDirection="row" padding={0} justifyContent="center">
-      {centerLinks.map((a, i) => {
-        return (
-          <Button
+          <Box
+            display="flex" 
+            alignContent="start"
+            // position="absolute"
+            // left="5px"
+          >
+
+
+          <img 
+            src={headerNWS} 
+            style={{
+              width: width < breakpoint ? '26.5px' : '100%',
+              height: width < breakpoint ? '30px' : '100%'
+            }}
+          />
+          <img 
+            src={headerNOAA} 
+            style={{
+              width: width < breakpoint ? '26.5px' : '100%',
+              height: width < breakpoint ? '30px' : '100%'
+            }}
+          />
+          </Box>
+         <Box display="flex" flexDirection="column" padding={0} marginLeft={2} >
+           <Typography
+            noWrap
+            component="a"
+            href="/"
             sx={{
-              fontSize: '0.7em',
+              fontFamily: 'Arial,sans-serif',
+              fontSize: width < breakpoint ? '1.0em' : '2.0em',
+              letterSpacing: '0px',
+              wordSpacing: '-0.5px',
+              textDecoration: 'none',
+              fontWeight: '550',
+              color: 'black',
+              textTransform: 'uppercase',
+              fontVariant: 'small-caps',
+            }}
+          >
+            Weather Prediction Center
+          </Typography>
+          <Typography
+            noWrap
+            component="a"
+            href="/"
+            sx={{
               fontVariant: 'small-caps',
               textTransform: 'uppercase',
               fontFamily: 'Arial,sans-serif',
+              fontSize:  width < breakpoint ? '0.35em' : '0.7em',
               textDecoration: 'none',
               color: 'gray',
-              letterSpacing: '2px',
-              padding: '0px',
-              margin: '0px 10px 0px 10px',
-              minWidth: '0px'
+              letterSpacing: width < breakpoint ? '1.5px' : '3px',
             }}
-            href={a[1]}>
-            {a[0]}
-          </Button>
-        )
-      })
+          >
+            National Oceanic and Atmospheric Administration
+          </Typography>
+        </Box>
+        <Box
+          // position="absolute"
+          // right="5px"
+        >
+          <img 
+            src={headerDOC} 
+            style={{
+              width: width < breakpoint ? '67.5px' : '100%',
+              height: width < breakpoint ? '30px' : '100%'
+            }}
+          />
+        </Box>
+      </Box>
+      <Divider />
+      <Box display="flex" flexDirection="row" padding={0} justifyContent="center">
+        {centerLinks.map((a, i) => {
+          return (
+            <Button
+              sx={{
+                fontSize:  width < breakpoint ? '0.35em' : '0.7em',
+                fontVariant: 'small-caps',
+                textTransform: 'uppercase',
+                fontFamily: 'Arial,sans-serif',
+                textDecoration: 'none',
+                color: 'gray',
+                letterSpacing: width < breakpoint ? '1px' : '2px',
+                padding: '0px',
+                margin: '0px 10px 0px 10px',
+                minWidth: '0px'
+              }}
+              href={a[1]}>
+              {a[0]}
+            </Button>
+          )
+        })
 
-      }
+        }
 
-    </Box>
-    <Divider />
-{/*    <Box>
-       <Toolbar disableGutters>
-       </Toolbar>
-    </Box>*/}
-  </AppBar>
-);
+      </Box>
+      <Divider />
+  {/*    <Box>
+         <Toolbar disableGutters>
+         </Toolbar>
+      </Box>*/}
+    </AppBar>
+  );
+}
 
 export default NavBar;
